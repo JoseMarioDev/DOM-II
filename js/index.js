6cbb60fbde98b8bf8d92766e89d6dab9b153e15f
Jose Montero Jr <josemariodev@gmail.com>
2019-05-15 07:36:20 -0500

    completed 2 event listeners. click, mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-// Your code goes here
+//1...change cursor to pointer and alert welcome on click
+const headerLogo = document.querySelector('.logo-heading');
+headerLogo.style.cursor = 'pointer';
+headerLogo.addEventListener('click', function(e) {
+  alert('welcome to the fun bus!!!!');
+});
+
+//2...mouseover links change color to red
+const navLink = document.querySelectorAll('nav a');
+console.log(navLink);
+navLink.forEach(function(el) {
+  el.addEventListener('mouseover', () => {
+    el.style.color = 'red';
+  });
+});
+
+//3...
