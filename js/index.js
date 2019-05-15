96184c87f83a71f3c07ea5a254741fd1879d3192
Jose Montero Jr <josemariodev@gmail.com>
2019-05-15 08:44:15 -0500

    added eventlisteners 3,4,5

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-//1...change cursor to pointer and alert welcome on click
+//1... click H1,change cursor to pointer and alert welcome
 const headerLogo = document.querySelector('.logo-heading');
 headerLogo.style.cursor = 'pointer';
 headerLogo.addEventListener('click', function(e) {
@@ -7,11 +7,27 @@ headerLogo.addEventListener('click', function(e) {
 
 //2...mouseover links change color to red
 const navLink = document.querySelectorAll('nav a');
-console.log(navLink);
 navLink.forEach(function(el) {
   el.addEventListener('mouseover', () => {
     el.style.color = 'red';
   });
 });
 
-//3...
+//3...double Click bus img. rotate 180deg
+const busImg = document.querySelector('.intro img');
+busImg.addEventListener('dblclick', () => {
+  busImg.style.transform = 'rotate(180deg)';
+});
+
+//4...mouse enter transform scale of pics
+const contentImg = document.querySelector('.img-content img');
+console.log(contentImg);
+contentImg.addEventListener('mouseenter', () => {
+  contentImg.style.transform = 'scale(1.25)';
+});
+
+//5... mouse leave scale pic back to original size
+contentImg.addEventListener('mouseleave', () => {
+  contentImg.style.transform = 'scale(1)';
+});
+g
